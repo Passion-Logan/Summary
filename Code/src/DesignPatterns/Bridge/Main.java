78f2b5acd94e37fb988bd3f2aa9fa7d9339015c6
Passion-Logan <wangdadamailbox@163.com>
2021-05-24 15:12:16 +0800

    update

---
+++ Code/src/DesignPatterns/Bridge/Main.java
@@ -0,0 +1,14 @@
+package DesignPatterns.Bridge;
+
+/**
+ * 桥接模式
+ *
+ * @author wql
+ * @desc Main
+ * @date 2021/5/24
+ * @lastUpdateUser wql
+ * @lastUpdateDesc
+ * @lastUpdateTime 2021/5/24
+ */
+public class Main {
+}
