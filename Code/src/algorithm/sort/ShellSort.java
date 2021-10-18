f8699545912b17670637eb7ce3693ba861698dc4
Passion-Logan <wangdadamailbox@163.com>
2021-10-18 15:58:19 +0800

    update

--- Code/src/algorithm/sort/ShellSort.java
+++ Code/src/algorithm/sort/ShellSort.java
@@ -34,14 +34,14 @@ public class ShellSort {
     public static int[] sort(int[] arr) {
         if (arr == null)
             return null;
-        int len = arr.length;//数组的长度
-        int k = len / 2;//初始的增量为数组长度的一半
+        int m = arr.length;//数组的长度
+        int k = m / 2;//初始的增量为数组长度的一半
         //while循环控制按增量的值来划不同分子序列，每完成一次增量就减少为原来的一半
         //增量的最小值为1，即最后一次对整个数组做直接插入排序
         while (k > 0) {
             //里边其实就是升级版的直接插入排序了，是对每一个子序列进行直接插入排序，
             //所以直接将直接插入排序中的‘1’变为‘k’就可以了。
-            for (int i = k; i < len; i++) {
+            for (int i = k; i < m; i++) {
                 int j = i;
                 int target = arr[i];
                 while (j >= k && target < arr[j - k]) {
